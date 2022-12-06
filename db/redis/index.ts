67c5332033ecfee7418a3750021aa8463e84cad1
Mark Artishuk <markyshuk@gmail.com>
2022-12-06 11:44:43 -0800

    Added redis port as an env variable

--- db/redis/index.ts
+++ db/redis/index.ts
@@ -3,9 +3,10 @@ import Redis from 'ioredis';
 const REDIS_URI = process.env.REDIS_URI as string;
 const REDIS_USERNAME = process.env.REDIS_USERNAME as string;
 const REDIS_PASSWORD = process.env.REDIS_PASSWORD as string;
+const REDIS_PORT = process.env.REDIS_PORT as string;
 
 const redis = new Redis({
-  port: 16930, // Redis port
+  port: parseInt(REDIS_PORT), // Redis port
   host: REDIS_URI, // Redis host
   username: REDIS_USERNAME, // needs Redis >= 6
   password: REDIS_PASSWORD,
